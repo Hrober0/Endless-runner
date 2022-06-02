f2cf533cd66356233dc5801daf8d847c9bccacbc
Hrober <bychrober@gmail.com>
2022-06-02 22:10:13 +0200

    Removed unused code from AudioClipSO

--- Scripts/Audio/AudioClipSO.cs
+++ Scripts/Audio/AudioClipSO.cs
@@ -13,21 +13,16 @@ namespace Audio
         [SerializeField, HideIf(nameof(HasMultipleClips))] private AudioClip clip;
         [SerializeField, ShowIf(nameof(HasMultipleClips))] private List<AudioClip> clips = new List<AudioClip>();
 
-        public const float VolumeMin = 0;
-        public const float VolumeMax = 1;
+
         [field: SerializeField, Range(VolumeMin, VolumeMax)] public float Volume { get; private set; } = 0.5f;
 
-        public const float PitchMin = -3;
-        public const float PitchMax = 3;
+
         [field: SerializeField, Range(PitchMin, PitchMax)] public float Pitch { get; private set; } = 1f;
-        public const float StereoPanMin = -1;
-        public const float StereoPanMax = 1;
+
         [field: SerializeField, Range(StereoPanMin, StereoPanMax)] public float StereoPan { get; private set; } = 0f;
-        public const float SpiralBlendMin = 0;
-        public const float SpiralBlendMax = 1;
+
         [field: SerializeField, Range(SpiralBlendMin, SpiralBlendMax)] public float SpiralBlend { get; private set; } = 0f;
-        public const float ReverbZoneMixMin = 0;
-        public const float ReverbZoneMixMax = 1.1f;
+
         [field: SerializeField, Range(ReverbZoneMixMin, ReverbZoneMixMax)] public float ReverbZoneMix { get; private set; } = 1f;
 
 
